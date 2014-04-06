7424322cd03df0deee840632f9cd3967fef8ee26
Zachary Mays <zackmays@gmail.com>
2014-04-06 16:53:44 -0400

    Change to HAML templates, add Bootstrap styles

--- routes.rb
+++ routes.rb
@@ -1,6 +1,18 @@
+require 'rubygems'
 require 'sinatra'
+require 'haml'
 
 get '/' do
   @title = 'Home'
-	erb :index
+	haml :index
+end
+
+get '/login' do
+  @title = 'Login'
+  haml :login
+end
+
+get '/admin' do
+  @title = 'Admin Portal'
+  haml :admin
 end
