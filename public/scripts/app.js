690f73881b47472cb6d1776d63208da1098bd400
Zachary Mays <zackmays@gmail.com>
2016-09-21 08:35:54 -0700

    Flushes out new projects and contacts sections

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,5 +7,11 @@ $(document).ready(function() {
         thisSection.hide('fade');
         newSection.show('fade');
         newSection.addClass('show');
-    })
+    });
+
+    $('.project-panel').click(function(e) {
+        e.preventDefault();
+        var destination = $(this).find('div').attr('title');
+        window.open(destination, '_blank');
+    });
 });
